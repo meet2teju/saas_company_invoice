238a908862265527b7bb4a32679146e99f0e1aa9
meet2teju <meet2teju@gmail.com>
2025-12-25 13:04:45 +0530

    invoice print issue

--- admin/invoice-details.php
+++ admin/invoice-details.php
@@ -455,12 +455,12 @@ function formatAmount($amount, $currencySymbol = null) {
 											<i class="isax isax-message-notif me-1"></i>Send Email
 										</a>
 
-									<a href="" class="btn btn-outline-white d-inline-flex align-items-center me-3" onclick="window.print(); return false;">
+									<!-- <a href="" class="btn btn-outline-white d-inline-flex align-items-center me-3" onclick="window.print(); return false;">
 										<i class="isax isax-printer me-1"></i>Print
-									</a>
-                                    <!-- <a href="print-invoice.php?id=<?= $invoiceId ?>" target="_blank" class="btn btn-outline-white d-inline-flex align-items-center me-3">
+									</a> -->
+                                    <a href="print-invoice.php?id=<?= $invoiceId ?>" target="_blank" class="btn btn-outline-white d-inline-flex align-items-center me-3">
                                         <i class="isax isax-printer me-1"></i>Print
-                                    </a> -->
+                                    </a>
 									<a href="#" class="btn btn-primary d-inline-flex align-items-center" data-bs-toggle="offcanvas" data-bs-target="#customcanvas">
 										<i class="isax isax-eye me-1"></i>View Details
 							
